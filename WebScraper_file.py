1f487f0a398e8c67affd34e8371b8ef95dccae5c
nelzon <niels.demeyer99@gmail.com>
2024-04-25 10:51:02 +0200

    constructed the url's

--- WebScraper_file.py
+++ WebScraper_file.py
@@ -1,17 +1,16 @@
-from playwright import sync_playwright
+from playwright.sync_api import sync_playwright
 
 class WebScraper:
     def __init__(self):
-        page = None
-        browser = None
         self.playwright = sync_playwright().start()
         self.browser = self.playwright.chromium.launch(headless=False)
         self.page = self.browser.new_page()
         self.provinces = ["oost-vlaanderen", "west-vlaanderen", "antwerpen", "limburg", "vlaams-brabant", "brussel", "waals-brabant", "henegouwen", "luik", "luxemburg", "namen"]
         self.provinces_links = {}
+
     def construct_links_per_province(self):
         # Get the links of the houses per provinces
         for i in self.provinces:
             url = f"https://www.immoweb.be/en/search/house/for-sale/{i}/province?countries=BE&page=1&orderBy=relevance"
             self.provinces_links[i] = url
-    
+        return self.provinces_links
