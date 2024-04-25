6c39170fbd565bf78c666a584a60753899976ced
nelzon <niels.demeyer99@gmail.com>
2024-04-25 10:38:49 +0200

    init webscraper

--- WebScraper_file.py
+++ WebScraper_file.py
@@ -2,4 +2,12 @@ from playwright import sync_playwright
 
 class WebScraper:
     def __init__(self):
+        page = None
+        browser = None
+        self.playwright = sync_playwright().start()
+        self.browser = self.playwright.chromium.launch(headless=False)
+        self.page = self.browser.new_page()
+        self.provinces = ["oost-vlaanderen", "west-vlaanderen", "antwerpen", "limburg", "vlaams-brabant", "brussel", "waals-brabant", "henegouwen", "luik", "luxemburg", "namen"]
+    def get_links_per_province(self):
+        # Get the links of the houses per provinces
         
