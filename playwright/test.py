8de0bc1788365995849ab63ce7f17c1972dcbe7b
Niels Demeyer <niels.demeyer99@gmail.com>
2024-04-28 14:48:22 +0200

    removed playwright

--- playwright/test.py
+++
@@ -1,8 +0,0 @@
-from WebScraper_file import WebScraper
-import os
-
-scraper = WebScraper()
-# scraper.construct_links_per_province()
-# scraper.scrape_provinces()
-results = scraper.scrape_individual_house("https://www.immoweb.be/en/classified/house/for-sale/deurle/9831/10884937")
-print(results)
