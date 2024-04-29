9b9c6d021d117cfb182e35fe168012390c98db44
nelzon <niels.demeyer99@gmail.com>
2024-04-29 12:28:00 +0200

    changes to save correct data

--- hrequests/main.py
+++ hrequests/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import glob
 from dotenv import load_dotenv
 import psycopg2
-
+from pprint import pprint
 load_dotenv()
 port = os.getenv("DB_PORT")
 password = os.getenv("DB_PASS")
@@ -26,8 +26,24 @@ single_listing_data = page.check_and_return_single_listing()
 
 # Convert the page data to a dictionary
 page_dict = page.to_dict()
+# pprint(page_dict)
 
+def remove_key_recursive(dict_, key_to_remove):
+    if isinstance(dict_, dict):
+        for key in list(dict_.keys()):
+            if key == key_to_remove:
+                dict_.pop(key)
+            else:
+                remove_key_recursive(dict_[key], key_to_remove)
+    elif isinstance(dict_, list):
+        for item in dict_:
+            remove_key_recursive(item, key_to_remove)
+
+# Now use this function to remove 'media' key from the dictionary
+remove_key_recursive(page_dict, 'media')
+print(page_dict)
 # Now you can use raw_data, single_listing_data, and page_dict as needed
 # For example, you can write them to a PostgreSQL database using the FileUtils class
 if single_listing_data is not None:
-    file_utils.write_dict_to_postgres('raw_data_table', single_listing_data)
+    file_utils.write_dict_to_postgres('raw_data_table', page_dict)
+    
