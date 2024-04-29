e69fd93b4c64e812542c21b7fbdfa873d45ba715
nelzon <niels.demeyer99@gmail.com>
2024-04-29 14:48:57 +0200

    integrated the check functions

--- hrequests/main.py
+++ hrequests/main.py
@@ -15,18 +15,6 @@ host = os.getenv("DB_HOST")
 
 # Create an instance of FileUtils
 file_utils = FileUtils()
-# Create an instance of ExtractPage with a URL
-page = ExtractPage("https://www.immoweb.be/en/classified/house/for-sale/sint-martens-latem/9830/11238297")
-
-# Get the raw data
-raw_data = page.get_raw_data()
-
-# Check if it's a single listing and get the raw data if it is
-single_listing_data = page.check_and_return_single_listing()
-
-# Convert the page data to a dictionary
-page_dict = page.to_dict()
-# pprint(page_dict)
 
 def remove_key_recursive(dict_, key_to_remove):
     if isinstance(dict_, dict):
@@ -39,11 +27,30 @@ def remove_key_recursive(dict_, key_to_remove):
         for item in dict_:
             remove_key_recursive(item, key_to_remove)
 
-# Now use this function to remove 'media' key from the dictionary
-remove_key_recursive(page_dict, 'media')
-print(page_dict)
-# Now you can use raw_data, single_listing_data, and page_dict as needed
-# For example, you can write them to a PostgreSQL database using the FileUtils class
-if single_listing_data is not None:
-    file_utils.write_dict_to_postgres('raw_data_table', page_dict)
-    
+# Get unchecked URLs
+unchecked_urls = file_utils.get_unchecked_urls()
+
+# Loop through each unchecked URL
+for url in unchecked_urls:
+    # Create an instance of ExtractPage with the URL
+    page = ExtractPage(url[0])  # url is a tuple, so we use url[0] to get the actual URL string
+
+    # Get the raw data
+    raw_data = page.get_raw_data()
+
+    # Check if it's a single listing and get the raw data if it is
+    single_listing_data = page.check_and_return_single_listing()
+
+    # Convert the page data to a dictionary
+    page_dict = page.to_dict()
+
+    # Remove 'media' key from the dictionary
+    remove_key_recursive(page_dict, 'media')
+
+    # Now you can use raw_data, single_listing_data, and page_dict as needed
+    # For example, you can write them to a PostgreSQL database using the FileUtils class
+    if single_listing_data is not None:
+        file_utils.write_dict_to_postgres('raw_data_table', page_dict)
+
+    # Set the URL as checked in the database
+    file_utils.set_checked(url[0])
