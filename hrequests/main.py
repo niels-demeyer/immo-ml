290cd8aac958f919e75ed41d609c9f7f8c2560a1
nelzon <niels.demeyer99@gmail.com>
2024-04-29 11:33:35 +0200

    adding connection support to dotenv

--- hrequests/main.py
+++ hrequests/main.py
@@ -3,6 +3,18 @@ from FileUtils_class import FileUtils
 import os
 import pandas as pd
 import glob
+from dotenv import load_dotenv
+import os
+import psycopg2
+
+load_dotenv()
+port = os.getenv("DB_PORT")
+password = os.getenv("DB_PASS")
+database = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+host = os.getenv("DB_HOST")
+
+
 
 # Create an instance of ExtractPage with a URL
 page = ExtractPage("https://www.immoweb.be/en/classified/house/for-sale/sint-martens-latem/9830/11238297")
