ef32a10ae75819e617153f360c67ec1eb77d2fb9
nelzon <niels.demeyer99@gmail.com>
2024-05-02 10:08:51 +0200

    Add pprint module and update data retrieval in main.py

--- ml/main.py
+++ ml/main.py
@@ -1,5 +1,6 @@
 from mlclass import MLClass
+from pprint import pprint
 
 ml = MLClass()
-data = ml.get_data("raw_data_table", "raw_property_type")
-print(data)
+data = ml.get_data("raw_data_table", "raw_property_type, raw_property_constructionpermit_floodzonetype, ")
+pprint(data)
